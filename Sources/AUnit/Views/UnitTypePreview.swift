a70cabc1ad55ca05e1c618f8641007e82d1039b3
RapboyGao <atgao74201@qq.com>
2024-07-14 13:42:50 +0800

    2024.07.14

--- Sources/AUnit/Views/UnitTypePreview.swift
+++ Sources/AUnit/Views/UnitTypePreview.swift
@@ -6,20 +6,20 @@
 //
 import SwiftUI
 
-@available(iOS 14.0, macOS 11.0, tvOS 17.0, *)
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 @available(watchOS, unavailable)
 private struct UnitTypesView: View {
     var body: some View {
         List(AUnitType.allCases, id: \.self) { unitType in
-            Menu(unitType.shortName) {
+
+            Section(unitType.shortName) {
                 UnitViews(filtererType: unitType)
             }
         }
     }
 }
 
-@available(iOS 14.0, macOS 11.0, tvOS 17.0, *)
-@available(watchOS, unavailable)
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 #Preview {
     Group {
         UnitTypesView()
